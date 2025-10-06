ac0393195b4489add11915758e928989344a18db
Stella-Long <huierl@nycstudents.net>
2025-10-06 09:25:10 -0700

    s

---
+++ Conditionals/index.js
@@ -0,0 +1,13 @@
+const button = document.querySelector(".btn");
+const box =document.querySelector(".box");
+function makeRGB(event) {
+
+    //console.log(event)
+    //console.log(event.target);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+button.addEventListener("click", makeRGB);
