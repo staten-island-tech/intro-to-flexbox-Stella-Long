e7a30b692256fad8340efc12a69cdaa14a7262d4
Stella-Long <huierl@nycstudents.net>
2025-10-23 09:33:50 -0700

    a

---
+++ logic.js
@@ -0,0 +1,25 @@
+let prod = {
+    title: "",
+    author: "",
+    genre: "",
+    year: ,
+};
+const cart = []
+function createCartObject(product){
+    const cartProduct = {...prod, quantity: 1 };
+}
+
+function checkCart(prod){
+    if (!prod) {
+        return;
+    }
+    const cartProduct = {...prod, quantity: 1 };
+    const found =cart.find((cartItem)=> cartItem.title === cartProduct.title);
+    if(found){
+        found.quantity += 1;
+    } else{
+        cart.push(cartProduct);
+    }
+}
+checkCart(prod);
+console.log(cart);
